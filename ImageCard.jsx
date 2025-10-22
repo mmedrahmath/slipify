2209f70d0f3e546872149c787e3005b8e37451a0
mmedrahmath <m.medrahmath@gmail.com>
2025-10-22 22:55:52 +0530

    Delete ImageCard.jsx

--- ImageCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function ImageCard({ url, title }) {
-  return (
-    <div className="rounded-xl overflow-hidden shadow-md hover:scale-105 transition">
-      <img src={url} alt={title} className="w-full h-auto" />
-    </div>
-  );
-}
